be4a66d28100826d71f7f4d4872dcefa27affaad
dmuralov <89232600+dmuralov@users.noreply.github.com>
2022-01-01 00:53:55 +0300

    Update index.js

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -52,7 +52,7 @@ bitcoind.on('ready', function() {
 
   var client = new bitcoin.Client({
     host: 'localhost',
-    port: 51835,
+    port: 52035,
     user: 'bitcoin',
     pass: 'local321'
   });
